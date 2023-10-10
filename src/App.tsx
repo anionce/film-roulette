1c2671ccf6e3355389d73f0c92b4c6428f402f90
Ana Gracia <ana.gracia@signaturit.com>
2023-10-10 16:20:18 +0200

    Ultimate details

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layout/MainLayout';
 import { NotFound } from './pages/NotFound/NotFound';
-import './App.scss';
 import React from 'react';
 import { Home } from './pages/Home/Home';
 
